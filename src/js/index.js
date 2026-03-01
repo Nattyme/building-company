cf0578f4f1e7051b3cc3ebd10f98ad8dccfa1bfe
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2026-03-02 02:42:49 +0300

    our-work: swiper base layout and settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
 // Галерея и лайтбоксы от Fancybox
 import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
-import { addSwiper } from 'modules/swiper.js';
-
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
-});
 
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
+import addSwiper from './modules/swiper.js';
 mobileNav();
 addSwiper();
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,24 +1,10 @@
 const addSwiper = function () {
   new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
     loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    speed: 2000,
+    spaceBetween: 40,
+    slidesPerView: 2,
   });
 }
 
